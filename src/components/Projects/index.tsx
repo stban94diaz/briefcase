6748036bb64ab209144ef863075e83a3c645a5c3
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2022-09-09 14:17:09 -0500

    :lipstick: Styles for many projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -7,10 +7,9 @@ function Projects() {
       <div className='flex justify-center mb-10'>
         <Project project={projectsData[0]} className='md:w-full lg:w-1/2' />
       </div>
-      <div className='flex justify-center gap-10'>
+      <div className='grid grid-cols-2 grid-flow-row gap-4'>
         {projectsData.slice(1).map((p, i) => (
           <Project
-            className='md:w-1/2 lg:w-1/4'
             key={i} project={p} />
         ))}
       </div>
--- src/services/mock-data.ts
+++ src/services/mock-data.ts
@@ -19,6 +19,20 @@ const projectsData = [
     description: "Vamos a aprender a crear NFT's dinámicos, lo que significa que pueden cambiar su metadata dependiendo de ciertas condiciones que definamos.",
     deploy: 'https://keeper-flower-react.vercel.app/',
     repo: 'https://github.com/stban94diaz/platziretoDNFT'
+  },
+  {
+    name: 'Solifity ETH Pokemon Factory',
+    object: 'Preto para realizar funciones a un modulo de solidity de pokemons Ethereum - Platzi',
+    description: 'Agrear tipos, debilidades, habilidades, requires y eventos al programa programado en Solidity.',
+    deploy: 'https://github.com/gelopfalcon/solidity-eth-challenge/pull/71',
+    repo: 'https://github.com/stban94diaz/solidity-eth-challenge'
+  },
+  {
+    name: 'React Curriculum vitae',
+    object: 'Realizar una app frontend con React JS',
+    description: 'Deploy, Document, Custom API, Integrate API, Create getData.js function, Add styles, Create the project components',
+    deploy: 'https://stban94diaz.github.io/react-eth-challenge/',
+    repo: 'https://github.com/stban94diaz/react-eth-challenge'
   }
 ]
 
